4627ac0ba561b51038589659876a9281ad1af14c
Sergey <exiton3@gmail.com>
2024-09-03 17:05:53 -0500

    Fixed PK and SK

--- src/AppFactory.Framework.DataAccess/Models/ModelBase.cs
+++ src/AppFactory.Framework.DataAccess/Models/ModelBase.cs
@@ -1,8 +1,13 @@
-﻿namespace AppFactory.Framework.DataAccess.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace AppFactory.Framework.DataAccess.Models;
 
 public class ModelBase
 {
+    [JsonPropertyName("PK")]
     public string PK { get; set; }
+
+    [JsonPropertyName("SK")]
     public string SK { get; set; }
 
     public string Id { get; set; }
